c6b50e16a17c0bf4bc7a343924426688d9e889b6
andzh1 <andrey.zhizhelev@yandex.ru>
2022-11-11 23:52:13 +0300

    Add getUserId

--- contracts/sbt.sol
+++ contracts/sbt.sol
@@ -173,4 +173,13 @@ contract SBT {
     {
         return souls[soulIdOfAddress[msg.sender]].hashedData;
     }
+
+    function getUserId(address _addr)
+        external
+        view
+        soulExists(soulIdOfAddress[_addr])
+        returns (uint)
+    {
+        return soulIdOfAddress[_addr];
+    }
 }
---
+++ tests/test_functions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import brownie
+from web3 import Web3
+from conftest import hashed_email, hashed_git
+
+
+def test_returns_usr_id(accounts, sbt):
+    sbt.mint(accounts[1], 123, {'from': accounts[0]})
+    sbt.claim([hashed_git, hashed_email], {'from': accounts[1]})
+
+    assert sbt.getUserId(accounts[1]) == 123
+
